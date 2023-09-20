e433ef4b9e8b0c286293d390c1c587bc01859fce
Rayyan <talentedmiseric1234@gmail.com>
2023-09-20 15:49:31 -0700

    Final Adjustments to GUI

--- GUI/mainGUI.py
+++ GUI/mainGUI.py
@@ -2,9 +2,15 @@ import sys
 from PyQt6 import QtCore, QtGui, QtWidgets
 import sqlite3
 from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QTabWidget, QWidget
+from PyQt6.QtWidgets import QVBoxLayout
 from PyQt6.QtGui import QAction
+import random
 
 class LocationResults(QtWidgets.QWidget):
+
+    locationClicked = QtCore.pyqtSignal(str, float)
+
     def __init__(self, matching_locations):
         super().__init__()
         self.setWindowTitle("Location Results")
@@ -17,6 +23,14 @@ class LocationResults(QtWidgets.QWidget):
             item = QtWidgets.QListWidgetItem(location)
             self.list_widget.addItem(item)
 
+        self.list_widget.itemClicked.connect(self.emitLocationClicked)
+
+    def emitLocationClicked(self, item):
+        location_name = item.text()
+        location_index = UserInfo.locations.index(location_name)
+        if location_index < len(UserInfo.distances):
+            distance = UserInfo.distances[location_index]
+            self.locationClicked.emit(location_name, distance)
 
 class UserInfo(QtWidgets.QWidget):
     def __init__(self, username):
@@ -56,6 +70,10 @@ class UserInfo(QtWidgets.QWidget):
 
         self.setStyleSheet("background-color: black;")
 
+        self.location_results = None
+        self.open_tabs = []
+        self.current_tab = None
+
     locations = ["Uberland Central Mall", "Grand Uber Mall", "Tim Hortons", "Starbucks", "Uber Cafe", "Uber Highschool", 
                  "Uber State University", "Uberland Insititute of Technology", "Uberland Park", "Uberland Zoo", "Uberland Airport", "Uberland Train Station",
                  "Uberland Bus Station", "Uberland City Hall", "Uberland Police Station", "Uberland Fire Station", "Uberland Hospital",
@@ -63,16 +81,79 @@ class UserInfo(QtWidgets.QWidget):
                  "Uber Bowling Alley", "Uberland Arcade", "Uberland Gym", "Uber Swimming Pool", "Uber Golf Course", "Uber Recreational Centre", 
                  "Cactus Club Restaurant", "Uber Bay Beach", "Uber Gas Station", "Uber Car Wash", "Uber Elementary School", "Uber Middle School", 
                  "Uber Supermarket", "Uberland Pharmacy", "Uber Central Bank", "Grand Uber Hotel"]
+    
+    distances = [3.2,5.1,13.1,14.0,2.2,3.0,7.3,8.9,4.5,10.6,11.2,1.1,1.5,2.7,4.2,5.3,6.5,7.7,6.4,9.2,5.7,22.5,16.6,14.6,19.2,8.7,2.9,1.9,4.5,16.3,21.1,23.0,25.6,11.2,7.0,9.9,32.1,5.2,9.0,13.7]
+    traffic = ["Heavy", "Medium", "Light"]
+    
+    def calculate_ride_cost(self, distance):
+        flat_fee = 5.0
+        per_increment_cost = 0.197
+        increment_size = 0.1
+
+        #Calculate the number of 0.1 increments in the given distance
+        increments = distance / increment_size
+
+        # Calculate the cost based on the flat fee and per increment cost
+        total_cost = flat_fee + increments * per_increment_cost
+
+        return total_cost
+
 
     def search(self):
         search_text = self.text_input.text()
         matching_locations = [location for location in self.locations if search_text.lower() in location.lower()]
         if matching_locations:
             self.location_results = LocationResults(matching_locations)
+            self.location_results.locationClicked.connect(self.openNewTab)
             self.location_results.show()
         else:
             QtWidgets.QMessageBox.information(self, "No Matches", "No matching locations found.")
         print("Searching for:", search_text)
+
+
+    def openNewTab(self, location_name, distance):
+        # Create and open a new tab with the selected location
+        traffic_level = random.choice(self.traffic)
+        if self.current_tab:
+            self.current_tab.close()
+        
+        if self.location_results:
+            new_tab = QtWidgets.QWidget()
+            new_tab.setWindowTitle(location_name)
+            self.current_tab = new_tab
+            location_index = self.locations.index(location_name)
+            if location_index < len(self.distances):
+                distance_km = self.distances[location_index]
+                cost = self.calculate_ride_cost(distance)
+
+                layout = QVBoxLayout(new_tab)
+                layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+
+                table_widget = QtWidgets.QTableWidget(3, 2, new_tab)
+                table_widget.setHorizontalHeaderLabels(["Description", "Value"])
+                table_widget.setRowHeight(2, 20)
+
+                table_widget.setItem(0, 0, QtWidgets.QTableWidgetItem("Distance (KM)"))
+                table_widget.setItem(0, 1, QtWidgets.QTableWidgetItem(f"{distance_km:.2f}"))
+                table_widget.setItem(1, 0, QtWidgets.QTableWidgetItem("Cost ($)"))
+                table_widget.setItem(1, 1, QtWidgets.QTableWidgetItem(f"${cost:.2f}"))
+                table_widget.setItem(2, 0, QtWidgets.QTableWidgetItem("Traffic Level"))
+                table_widget.setItem(2, 1, QtWidgets.QTableWidgetItem(traffic_level))
+                
+
+                table_widget.setStyleSheet("QTableWidget { background-color: white; }")
+                table_widget.horizontalHeader().setStyleSheet("QHeaderView::section { background-color: lightblue; }")
+                table_widget.verticalHeader().setStyleSheet("QHeaderView::section { background-color: lightblue; }")
+                
+
+                table_widget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeMode.ResizeToContents)
+                layout.addWidget(table_widget)
+                new_tab.setStyleSheet("background-color: lightgray;")
+                table_widget.setStyleSheet("QTableWidget { background-color: white; }")
+
+            # Add any widgets or content you want for the new tab here
+            new_tab.show()
+            self.open_tabs.append(new_tab)
 
     def logout(self):
         self.close()
@@ -325,6 +406,7 @@ class Newuser(QtWidgets.QWidget, Ui_NewUser):
         self.create_db_newuser()
 
     def back_handler(self):
+        self.close()
         self.switch_window.emit()
 
     def create_db_newuser(self):
